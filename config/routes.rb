7a792fb019f3ff6a567c07687eb0c5b277b77d74
daiki2828 <114804118+daiki2828@users.noreply.github.com>
2022-12-06 09:02:00 +0000

    device導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :admins,  skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+  devise_for :users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+scope module: :public do
+    root :to => "homes#top"
+end
+
+namespace :admin do
+  get "/" => "homes#top"
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
