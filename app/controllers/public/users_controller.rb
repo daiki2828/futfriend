403396c64f166ca83d7d9ac2ba64af0b68829bf6
daiki2828 <114804118+daiki2828@users.noreply.github.com>
2022-12-14 03:34:40 +0000

    [add]レイアウト(投稿機能・ユーザー詳細)

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,6 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
   before_action :search
   
   def search
@@ -14,11 +16,36 @@ class Public::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end 
+  
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end 
 
   def favorites
     @user = User.find(params[:id])
     favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
     @favorite_posts = Post.find(favorites)
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile, :profile_image, :prefacture_name)
+  end
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_11_122319) do
+ActiveRecord::Schema.define(version: 2022_12_13_083012) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -132,6 +132,7 @@ ActiveRecord::Schema.define(version: 2022_12_11_122319) do
     t.integer "user_status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "favorite_team"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
